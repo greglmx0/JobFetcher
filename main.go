39ea86ac82344cb8e52b4c55fe30950ae6bf1ef9
Gregoire Lamoureux <gregoire.lamoureux@eiffage.com>
2025-03-05 11:08:48 +0100

    feat: initialize cron job scheduling and improve error handling in main function for website

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
+	"JobFetcher/internal/cron"
 	"JobFetcher/internal/db"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -14,30 +14,22 @@ import (
 	"JobFetcher/internal/usecase"
 
 	"github.com/gorilla/mux"
-	"github.com/robfig/cron"
 )
 
 func main() {
-
 	// get env variables TELEGRAM_BOT_TOKEN
 	telegramToken := os.Getenv("TELEGRAM_BOT_TOKEN")
 	telegramChatIDStr := os.Getenv("TELEGRAM_CHAT_ID")
 	telegramChatID, err := strconv.ParseInt(telegramChatIDStr, 10, 64)
+	if err != nil {
+		log.Fatalf("Erreur lors de la conversion de TELEGRAM_CHAT_ID: %v", err)
+	}
 
 	log.Println("Initialisation du bot Telegram")
 	telegramBot, err := telegram.NewTelegramBot(telegramToken)
-
-	// Initialiser le planificateur de tâches cron
-	c := cron.New()
-	i := 0
-	c.AddFunc("*/5 * * * * *", func() {
-		i++
-		log.Printf("Hello, world %d!", i)
-
-		message := "Hello, world " + fmt.Sprint(i) + " !"
-		telegramBot.SendMessage(int64(telegramChatID), message)
-	})
-	c.Start()
+	if err != nil {
+		log.Fatalf("Erreur lors de l'initialisation du bot Telegram: %v", err)
+	}
 
 	// Initialiser la base de données
 	db, err := db.InitDB("/app/data/jobfetcher.db")
@@ -59,13 +51,13 @@ func main() {
 	r.HandleFunc("/user/{id:[0-9]+}", userHandler.GetUserHandler).Methods("GET")
 	r.HandleFunc("/users", userHandler.GetAllUsersHandler).Methods("GET")
 	r.HandleFunc("/user", userHandler.CreateUserHandler).Methods("POST")
-
 	r.HandleFunc("/website", websiteHandler.CreateWebsiteHandler).Methods("POST")
 	r.HandleFunc("/websites", websiteHandler.GetAllWebsitesHandler).Methods("GET")
+	r.HandleFunc("/website/{id:[0-9]+}", websiteHandler.DeleteWebsiteHandler).Methods("DELETE")
+
+	// Initialiser le planificateur de tâches cron
+	cron.InitCron(telegramBot, telegramChatID, webSiteRepo)
 
 	log.Println("Serveur en cours d'exécution sur le port 8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
-
-	// Bloquer indéfiniment pour garder le planificateur cron en cours d'exécution
-	select {}
 }
